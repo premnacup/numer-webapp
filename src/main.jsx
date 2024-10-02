83330bb8cccc597851559bd5059c7c6b52254cfb
premnacup <premprangja@gmail.com>
2024-10-02 21:42:32 +0700

    Test FalsePosition

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import Navbar from "./components/Navbar.jsx";
+import App from "./App.jsx";
 import Bisection from "./Bisection.jsx";
+import FalsePosition from "./FalsePosition.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Navbar />
     {/* <App /> */}
-    <Bisection />
+    {/* <Bisection /> */}
+    <FalsePosition />
   </BrowserRouter>
 );

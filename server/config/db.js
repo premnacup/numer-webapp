0630254303f604334e949f864c80bcaddb16f00d
premnacup <premprangja@gmail.com>
2024-11-02 04:27:46 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config(); // Only needed for local development
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    console.log("Connecting to MongoDB with URI:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
